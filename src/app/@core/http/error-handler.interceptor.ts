e1200448352a54b18bd36264dde4114bde7437b7
Yash Anand <yashanand1910@gmail.com>
2020-08-24 08:37:11 +0530

    chore: fix problems

--- src/app/@core/http/error-handler.interceptor.ts
+++ src/app/@core/http/error-handler.interceptor.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
-import { Logger } from '../logger.service';
+import { Logger } from '@app/@core';
 
 const log = new Logger('ErrorHandlerInterceptor');
 
@@ -15,16 +15,16 @@ const log = new Logger('ErrorHandlerInterceptor');
   providedIn: 'root',
 })
 export class ErrorHandlerInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(catchError((error) => this.errorHandler(error)));
-  }
-
   // Customize the default error handler here if needed
-  private errorHandler(response: HttpEvent<any>): Observable<HttpEvent<any>> {
+  private static errorHandler(response: HttpEvent<any>): Observable<HttpEvent<any>> {
     if (!environment.production) {
       // Do something with the error
       log.error('Request error', response);
     }
     throw response;
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError((error) => ErrorHandlerInterceptor.errorHandler(error)));
   }
 }
--- src/app/@shared/loader/loader.component.spec.ts
+++ src/app/@shared/loader/loader.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoaderComponent } from './loader.component';
+import { LoaderComponent } from '@shared';
 
 describe('LoaderComponent', () => {
   let component: LoaderComponent;
