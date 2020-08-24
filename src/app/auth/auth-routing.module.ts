c45e7919cb29a7aa7d4a5853e200d5528f904043
Yash Anand <yashanand1910@gmail.com>
2020-08-24 16:15:25 +0530

    chore: setup

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -7,11 +7,6 @@ import { LoginComponent } from './login.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
     component: LoginComponent,
     data: { title: extract('Login') },
   },
