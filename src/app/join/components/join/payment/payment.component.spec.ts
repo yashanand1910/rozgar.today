1d499d59929762eb3c7765e6e0813bdd066bec19
Yash Anand <yashanand1910@gmail.com>
2020-10-20 19:03:50 +0530

    chore: setup
    Signed-off-by: Yash Anand <yashanand1910@gmail.com>

--- src/app/join/components/join/payment/payment.component.spec.ts
+++ src/app/join/components/join/payment/payment.component.spec.ts
@@ -2,6 +2,7 @@ import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaymentComponent } from './payment.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 describe('PaymentComponent', () => {
   let component: PaymentComponent;
@@ -10,6 +11,7 @@ describe('PaymentComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
+        imports: [NzGridModule],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
         declarations: [PaymentComponent]
       }).compileComponents();
